5f2cd70e6a54b145a19a69a0d2b8a4b76e34242e
unknown <Etudiant@5357320350.campus.univ.run>
2021-04-12 10:12:46 +0400

    # update

--- Claire_script_secr/Claire_MULTI-SESSION_analysis.r
+++ Claire_script_secr/Claire_MULTI-SESSION_analysis.r
@@ -84,27 +84,35 @@ fit0 <- secr::secr.fit(d,
 fit1 <- secr::secr.fit(d,
                        mask = ovmask,
                        model = g0 ~ 1)
-# Detection probability varies with individual behaviour = learning reponse (b)
+# Detection probability constant across animals, occasions and detectors
 fit2 <- secr::secr.fit(d,
                        mask = ovmask,
-                       model = g0 ~ b)
-# learn reponse affects both g0 and sigma
+                       model = sigma ~ 1)
+# Detection probability varies with individual behaviour = learning reponse (b)
 fit3 <- secr::secr.fit(d,
+                       mask = ovmask,
+                       model = g0 ~ b)
+# Detection probability varies with individual behaviour = learning reponse (b)
+fit4 <- secr::secr.fit(d,
+                       mask = ovmask,
+                       model = sigma ~ b)
+# learn reponse affects both g0 and sigma
+fit5 <- secr::secr.fit(d,
                        mask = ovmask,
                        model = list(g0 ~ b, sigma ~ b))
 # Detection probability varies with individuals learning and time (pour savoir si les cages deviennent attractante seulement au bout de plusieurs jours après installation)
-fit4 <- secr::secr.fit(d,
+fit6 <- secr::secr.fit(d,
                        mask = ovmask,
                        model = list(g0 ~ b + T))
 # Session
-fit5 <- secr::secr.fit(d,
-                       mask = ovmask,
-                       model = list(D ~ session, g0 ~ session, sigma ~ session))
+# fit5 <- secr::secr.fit(d,
+#                        mask = ovmask,
+#                        model = list(D ~ session, g0 ~ session, sigma ~ session))
 
 # Session
-fit6 <- secr::secr.fit(d,
+fit7 <- secr::secr.fit(d,
                        mask = ovmask,
                        model = list(D ~ 1, g0 ~ session, sigma ~ session))
 
 ##### COMPARAISON DE TOUS LES MODELES ####
-AIC(fit0, fit1, fit2, fit3, fit4, fit5, fit6)
+AIC(fit0, fit1, fit2, fit3, fit4, fit5, fit6, fit7)
