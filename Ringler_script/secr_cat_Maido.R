8ef8bab74efe251ce7685109c5ebf8b3f75a559c
unknown <Etudiant@5357320350.campus.univ.run>
2021-04-12 11:52:55 +0400

    # Cleaned & annotated

--- Ringler_script/secr_cat_Maido.R
+++ Ringler_script/secr_cat_Maido.R
@@ -3,58 +3,123 @@
 # 2 sessions, 30 jours, 20/21 cameras traps
 # CLOSED = foret, hos sentier
 # OPEN = sentiers 
+#-----------------------------#
+#### Multi-session models ####
+#---------------------------#
 
+rm(list = ls())
+
+capt <- "C:/Users/Etudiant/Desktop/SMAC/GITHUB/CATTRAP/Claire_script_secr/DATA_pour_David/Capture.txt"
+
+
+trapfile1 <- "C:/Users/Etudiant/Desktop/SMAC/GITHUB/CATTRAP/Claire_script_secr/DATA_pour_David/Trap_MULTI-SESSION_OPEN.txt"
+trapfile2 <- "C:/Users/Etudiant/Desktop/SMAC/GITHUB/CATTRAP/Claire_script_secr/DATA_pour_David/Trap_MULTI-SESSION_CLOSED.txt"
+
+Ha <- maptools::readShapeSpatial("C:/Users/Etudiant/Desktop/SMAC/GITHUB/CATTRAP/Claire_script_secr/Habitat")
 
 library(secr)
-load(".RData")
+#load(".RData")
 
 
 ### secr models
-cat_maido<-read.capthist(file.choose(),c(file.choose(),file.choose()),
-		fmt="trapID",
-		#covnames=c("sex","age","group"),
-		#trapcovnames=c("device"),
-		detector='proximity'
-		)
-cat_maido<-shareFactorLevels(cat_maido)
+# cat_maido <- read.capthist(capt,c(trapfile2, trapfile1),
+#                            fmt = "trapID",
+#                            #covnames=c("sex","age","group"),
+#                            #trapcovnames=c("device"),
+#                            detector = "proximity")
+
+cat_maido <- read.capthist(file.choose(),c(file.choose(),file.choose()),
+                           fmt = "trapID",
+                           #covnames=c("sex","age","group"),
+                           #trapcovnames=c("device"),
+                           detector = "proximity")
+
+
+cat_maido <- shareFactorLevels(cat_maido)
 
 
 # halfnormal dfn
-Mcat01<-secr.fit(cat_maido, model = list(D~session,g0~1,sigma~1),detectfn=0,CL=F, buffer=3000, verify=F)
+Mcat01 <- secr.fit(cat_maido,
+                   model = list(D~session, g0~1, sigma~1),
+                   detectfn = 0, # halfnormal
+                   CL = F,
+                   buffer = 3000,
+                   verify = F)
 # D ~ 0.18, 0.15
 # g0 ~ 0,07
 # sigma ~ 900
 
 # hazard dfn
-Mcat02<-secr.fit(cat_maido, model = list(D~session,g0~1,sigma~1,z~1),detectfn=1,CL=F, buffer=3000, verify=F)
+# In help of the function - "the "hazard-rate" detection functions described by Hayes and Buckland (1983), are not recommended for SECR because of their long tail"
+Mcat02 <- secr.fit(cat_maido,
+                   model = list(D~session, g0~1, sigma~1, z~1),
+                   detectfn = 1, # hazard rate
+                   CL = F,
+                   buffer = 3000,
+                   verify = F)
 # D ~ 0.21, 0.17
 # g0 ~ 0,08
 # sigma ~ 820
 
-Mcat04<-secr.fit(cat_maido, model = list(D~1,g0~1,sigma~1,z~1),detectfn=1,CL=F, buffer=3000, verify=F)
+Mcat04 <- secr.fit(cat_maido,
+                   model = list(D~1, g0~1, sigma~1, z~1),
+                   detectfn = 1, # hazard rate
+                   CL = F,
+                   buffer = 3000,
+                   verify = F)
 # D ~ 0.19/km2
 # g0 ~ 0,08
 # sigma ~ 820m
 # z ~ 3.88
+
 # Home range 95% and 50%
-HR95<-3.14*((circular.r(p=0.95,detectfn='HR',detectpar=list(sigma=1,z=3.88)))*820)^2
+HR95 <- 3.14*((circular.r(p = 0.95,
+                          detectfn = 'HR', # hazard rate
+                          detectpar = list(sigma = 1, z = 3.88)))*820)^2
 # HR95 = 7.9 km2
-HR50<-3.14*((circular.r(p=0.5,detectfn='HR',detectpar=list(sigma=1,z=3.88)))*820)^2
+HR50 <- 3.14*((circular.r(p = 0.5,
+                          detectfn = 'HR', # hazard rate
+                          detectpar = list(sigma = 1, z = 3.88)))*820)^2
 # HR50 = 0.7 km2
 
-Mcat05<-secr.fit(cat_maido, model = list(D~1,g0~session,sigma~1,z~1),detectfn=1,CL=F, buffer=3000, verify=F)
+Mcat05 <- secr.fit(cat_maido,
+                   model = list(D~1, g0~session, sigma~1, z~1),
+                   detectfn = 1, # hazard rate
+                   CL = F,
+                   buffer = 3000,
+                   verify = F)
 
 
-Mcat06<-secr.fit(cat_maido, model = list(D~1,g0~1,sigma~session,z~1),detectfn=1,CL=F, buffer=3000, verify=F)
+Mcat06 <- secr.fit(cat_maido,
+                   model = list(D~1, g0~1, sigma~session, z~1),
+                   detectfn = 1, # hazard rate
+                   CL = F,
+                   buffer = 3000,
+                   verify = F)
 # D ~ 0.19
 # g0 ~ 0,08
 # sigma ~ 820
 
-Mcat07<-secr.fit(cat_maido, model = list(D~1,g0~session,sigma~session,z~1),detectfn=1,CL=F, buffer=3000, verify=F)
+Mcat07 <- secr.fit(cat_maido,
+                   model = list(D~1, g0~session, sigma~session, z~1),
+                   detectfn = 1, # hazard rate
+                   CL = F,
+                   buffer = 3000,
+                   verify = F)
 
-Mcat08<-secr.fit(cat_maido, model = list(D~1,g0~1,sigma~session,z~session),detectfn=1,CL=F, buffer=3000, verify=F)
+Mcat08 <- secr.fit(cat_maido,
+                   model = list(D~1, g0~1, sigma~session, z~session),
+                   detectfn = 1, # hazard rate
+                   CL = F,
+                   buffer = 3000,
+                   verify = F)
 
-Mcat09<-secr.fit(cat_maido, model = list(D~1,g0~1,sigma~1,z~session),detectfn=1,CL=F, buffer=3000, verify=F)
+Mcat09 <- secr.fit(cat_maido,
+                   model = list(D~1, g0~1, sigma~1, z~session),
+                   detectfn = 1, # hazard rate
+                   CL = F,
+                   buffer = 3000,
+                   verify = F)
 
 
 AIC(Mcat01,Mcat02,Mcat04,Mcat05,Mcat06,Mcat07,Mcat08,Mcat09)
