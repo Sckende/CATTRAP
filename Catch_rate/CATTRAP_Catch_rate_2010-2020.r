1aca64659b6e1e47adbac920142df058361bd646
unknown <Etudiant@5357320350.campus.univ.run>
2021-05-05 15:16:00 +0400

    # Cat capture explo global & Local (GBR) data

--- Catch_rate/CATTRAP_Catch_rate_2010-2020.r
+++ Catch_rate/CATTRAP_Catch_rate_2010-2020.r
@@ -1,34 +1,40 @@
-#### Exploration & analysis of trapped cats ####
+#### Exploration & analysis of trapped cats data ####
+rm(list = ls())
 
-cat_trap <- read.table("C:/Users/Etudiant/Desktop/SMAC/GITHUB/CATTRAP/Catch_rate/CATTRAP_Bilan_2010-2020.txt", h = T, dec = ",", sep = "\t")
-dim(cat_trap)
-summary(cat_trap)
+cat <- read.table("C:/Users/Etudiant/Desktop/SMAC/GITHUB/CATTRAP/Catch_rate/CATTRAP_Bilan_2010-2020.txt", h = T, dec = ",", sep = "\t")
+dim(cat)
+summary(cat)
 
-for(i in 1:6){
+# Keeping the CAGE type only
+cat_trap <- cat[cat$dispo_type == "CAGE",]
+
+#### EXPLORATION ####
+for(i in c(2, 4:7)){
 print(table(cat_trap[,i], useNA = "always"))
 }
 
-
+# ---- Exploration of captured cat number ---- 
 tapply(cat_trap$trap_cat, cat_trap$season, sum)
 tapply(cat_trap$trap_cat, cat_trap$season_year, sum)
 tapply(cat_trap$trap_night, cat_trap$season_year, sum)
 
-# Keeping the CAGE type only
-cat_trap <- cat_trap[cat_trap$dispo_type == "CAGE",]
 
 # AV2M applied the new protocol only from 2019
 table(cat_trap$structure, cat_trap$season_year)
 
-
+# ---- Computation of raw capture rate/season_year ----
 require(dplyr)
 rate <- cat_trap %>%
   dplyr::group_by(season_year) %>%
   dplyr::summarise(total_cat_trap = sum(trap_cat), total_trap_night = sum(trap_night), catch_rate = sum(trap_cat)/sum(trap_night))
 
-plot(rate$season_year, rate$catch_rate, bty = "n")
+barplot(rate$catch_rate, bty = "n", col = rainbow(length(rate$season_year)), names.arg = rate$season_year)
 
-rate$season_number <- 1:nrow(rate)
-rate$protocol <- c(rep("old", 6), rep("new", 4))
+# ---- Categorization of protocol type 'old' vs. 'new' ----
+# rate$season_number <- 1:nrow(rate)
+# rate$protocol <- c(rep("old", 6), rep("new", 4))
+# ***WARNING*** - Be carefull because application year of new protocol was different depending on the structure
+
 
 #### EXPLO ANALYSIS ####
 library("lme4") # For generalised linear models
@@ -73,3 +79,63 @@ lm1 <- stats::glm(rate$total_cat_trap ~ rate$season_number,
                   offset = log(rate$total_trap_night))
 summary(lm1)
 plot(gam1, page = 1, all.terms = TRUE, residuals = TRUE)
+
+#### GLOBAL SPATIAL VISUALISATION ####
+
+# ---- Choosing a color range ----
+rainb_col <- rainbow(n = length(unique(cat_trap$site_code)))
+cat_trap$site_color <- rainb_col[as.numeric(as.factor(cat_trap$site_code))]
+
+# ---- Data explo ----
+count <- table(cat_trap$site_code)
+barplot(count, col = rainb_col[as.numeric(as.factor(unique(cat_trap$site_code)))])
+
+# ---- Data visualization ----
+plot(cat_trap$lon,
+     cat_trap$lat,
+     pch = 20,
+     col = rainb_col[as.numeric(as.factor(cat_trap$site_code))],
+     cex = 2)
+
+#legend("topright", legend = unique(cat_trap$site_code), col = rainb_col[unique(cat_trap$site_code_number)], pch = 20)
+
+points(cat_trap$lon[cat_trap$site_code == "GBR"],
+     cat_trap$lat[cat_trap$site_code == "GBR"],
+     pch = 20,
+     #col = rainb_col[cat_trap$site_code_number[cat_trap$site_code == "AZO"]]
+     col = "black")
+
+#### GBR SPATIAL VISUALISATION ####
+
+# ---- Data subset ---- 
+# with site_code = "GBR"
+GBR_trap <- cat_trap[cat_trap$site_code == "GBR",]
+
+# ---- Working on localities ---- 
+table(GBR_trap$local, useNA = 'always')
+
+# ----  Cleaning data ---- 
+GBR_trap$local[stringr::str_detect(GBR_trap$local,
+                             'Piste for') == TRUE] <- 'Piste forestiere Maido'
+
+# ---- Different color for each locality ---- 
+unique(GBR_trap$local)
+rainb_colGBR <-rainbow(length(unique(GBR_trap$local)))
+
+# ---- Visualization ---- 
+plot(GBR_trap$lon,
+     GBR_trap$lat,
+     pch = 20,
+     col = rainb_colGBR[as.numeric(as.factor(GBR_trap$local))],
+     cex = 2)
+points(GBR_trap$lon[GBR_trap$local == "Piste de La Glacière"],
+       GBR_trap$lat[GBR_trap$local == "Piste de La Glacière"],
+       pch = 20,
+       col = "black") # Here problem with category of localities for each GPS points
+
+# *** ==> necessity of delimitation with a polygon delimited the Maido area ***
+# Use the shapefile !!!
+
+#### EXTRACTION OF GRAND BENARRE DATABASE ####
+
+# write.table(GBR_trap, 'C:/Users/Etudiant/Desktop/SMAC/GITHUB/CATTRAP/Catch_rate/CATTRAP_GBR_data.txt', sep = '\t', dec = '.')
