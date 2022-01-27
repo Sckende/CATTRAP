7066fe0b0d98a556f505e1269c1f61adc542e7ae
Sckende <clrccljuhasz@gmail.com>
2022-01-27 10:33:42 +0400

    # Trying several VSCode extensions

--- Claire_script_secr/GLOBAL_option-ABANDONNED/GLOBAL_option_models.r
+++ Claire_script_secr/GLOBAL_option-ABANDONNED/GLOBAL_option_models.r
@@ -5,54 +5,111 @@
 # two models, one per habitat
 rm(list = ls())
 
+require(secr)
+
 getwd()
-setwd("C:/Users/Etudiant/Desktop/SMAC/GITHUB/CATTRAP/Claire_script_secr")
+# setwd("C:/Users/Etudiant/Desktop/SMAC/GITHUB/CATTRAP/Claire_script_secr")
 
 # Capture file
-capt <- "C:/Users/Etudiant/Desktop/SMAC/GITHUB/CATTRAP/Claire_script_secr/capture_GLOBAL_option.txt"
+capt <- "C:/Users/ccjuhasz/Desktop/SMAC/GITHUB/CATTRAP/Claire_script_secr/GLOBAL_option-ABANDONNED/capture_GLOBAL_option.txt"
 # Trap file
-trap <- "C:/Users/Etudiant/Desktop/SMAC/GITHUB/CATTRAP/Claire_script_secr/trap_GLOBAL_option5.txt"
+trap <- "C:/Users/ccjuhasz/Desktop/SMAC/GITHUB/CATTRAP/Claire_script_secr/GLOBAL_option-ABANDONNED/trap_GLOBAL_option5.txt"
 
 # Data check
 count.fields(capt)
 count.fields(trap)
 
-CM <- secr::read.capthist(capt, trap, covnames = "HABITAT", detector = "count", noccasions = 63)
+# SECR file creation
+CM <- secr::read.capthist(capt,
+                          trap,
+                          covnames = "HABITAT",
+                          detector = "count",
+                          noccasions = 63)
 
+# DL the mask 
+nbg.sp <- readRDS('C:/Users/ccjuhasz/Desktop/SMAC/GITHUB/CATTRAP/Claire_script_secr/CATTRAP_NEW_MASK.rds')
+mapview::mapview(nbg.sp)
+cat_mask <- make.mask(traps(CM),
+                      type = 'trapbuffer',
+                      buffer = 3000,
+                      poly = nbg.sp)
+plot(cat_mask)
+plot(traps(CM))
 
-# fit0 <- secr::secr.fit(CM,
-#                             model = list(D~1, g0~1, sigma~1),
-#                             mask = NULL)
+#####################################################################
+# Selection of the detection function - half-normal vs. hazard rate #
+#####################################################################
 
+fit0 <- secr::secr.fit(CM,
+                       model = list(D~1, g0~1, sigma~1),
+                       mask = cat_mask,
+                       detectfn = 1, # hazard rate
+                       CL = F,
+                       verify = F) # ---> best detection function
+
+fit00 <- secr::secr.fit(CM,
+                       model = list(D~1, g0~1, sigma~1),
+                       mask = cat_mask,
+                       detectfn = 0, # half-normal
+                       CL = F,
+                       verify = F)
+
+AIC(fit0, fit00)
+#                      model    detectfn npar    logLik     AIC    AICc dAICc AICcwt
+# fit0  D~1 g0~1 sigma~1 z~1 hazard rate    4 -146.4083 300.817 308.817 0.000 0.7342
+# fit00     D~1 g0~1 sigma~1  halfnormal    3 -150.4243 306.849 310.849 2.032 0.2658
+
+###################################
+# Group (cloded vs. open) effects #
+###################################
 fit1 <- secr::secr.fit(CM,
                        model = list(D~1, g0~1, sigma~1),
                        groups = "HABITAT",
-                       mask = NULL)
+                       mask = cat_mask,
+                       detectfn = 1, # hazard rate
+                       CL = F,
+                       verify = F)
 
 fit2 <- secr::secr.fit(CM,
                        model = list(D~g, g0~1, sigma~1),
                        groups = "HABITAT",
-                       mask = NULL)
+                       mask = cat_mask,
+                       detectfn = 1, # hazard rate
+                       CL = F,
+                       verify = F)
 
 fit3 <- secr::secr.fit(CM,
                        model = list(D~g, g0~g, sigma~1),
                        groups = "HABITAT",
-                       mask = NULL)
+                       mask = cat_mask,
+                       detectfn = 1, # hazard rate
+                       CL = F,
+                       verify = F)
 fit4 <- secr::secr.fit(CM,
                        model = list(D~g, g0~g, sigma~g),
                        groups = "HABITAT",
-                       mask = NULL)
+                       mask = cat_mask,
+                       detectfn = 1, # hazard rate
+                       CL = F,
+                       verify = F)
 fit5 <- secr::secr.fit(CM,
                        model = list(D~1, g0~g, sigma~g),
                        groups = "HABITAT",
-                       mask = NULL)
-# fit6 <- secr::secr.fit(CM,
-#                        model = list(D~1, g0~g, sigma~g),
-#                        groups = "HABITAT",
-#                        mask = NULL,
-#                        buffer = 1000)
+                       mask = cat_mask,
+                       detectfn = 1, # hazard rate
+                       CL = F,
+                       verify = F)
 
-require(secr)
+
 AIC(fit1, fit2, fit3, fit4, fit5)
 
 summary(fit5)
+
+
+
+
+
+
+
+
+
