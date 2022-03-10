ce06f3a16c0e7d900c31c889cb82369e1cc687ec
Sckende <clrccljuhasz@gmail.com>
2022-03-10 15:23:06 +0400

    # Last version of models

--- Claire_script_secr/CATTRAP_ms_secr_analyses.r
+++ Claire_script_secr/CATTRAP_ms_secr_analyses.r
@@ -37,18 +37,9 @@ mapview(grid_sp,
 capt <- "C:/Users/ccjuhasz/Desktop/SMAC/GITHUB/CATTRAP/Claire_script_secr/GLOBAL_option-ABANDONNED/capture_GLOBAL_option.txt"
 
 # -----> Trap file
-trap <- "C:/Users/ccjuhasz/Desktop/SMAC/GITHUB/CATTRAP/Claire_script_secr/GLOBAL_option-ABANDONNED/trap_GLOBAL_option5.txt"
+trap <- "C:/Users/ccjuhasz/Desktop/SMAC/GITHUB/CATTRAP/Claire_script_secr/GLOBAL_option-ABANDONNED/trap_GLOBAL_option5_with_covar.txt"
 
-# trap_file <- read.traps(file = trap,
-#                         detector = "count",
-#                         covnames = "hab_type",
-#                         binary.usage = TRUE,
-#                         trapID = "Detector")
-# CM2 <- make.capthist(captures = capt,
-#                      traps = trap_file,
-#                      fmt = "trapID",
-#                      noccasions = 63)
-# summary(trap_file)
+
 # -----> Data check
 count.fields(capt)
 count.fields(trap)
@@ -59,7 +50,8 @@ CM <- secr::read.capthist(capt,
                           fmt = "trapID",
                           trapcovnames = "hab_type",
                           detector = "count",
-                          noccasions = 63)
+                          noccasions = 63,
+                          verify = T)
 
 usage(traps(CM))
 summary(CM)
@@ -121,80 +113,95 @@ traps(CM)
 # Selection of the detection function - half-normal vs. hazard rate ####
 ########################################################################
 
-fit0 <- secr::secr.fit(CM,
+fit0 <- secr::secr.fit(CM1,
                        model = list(D~1, g0~1, sigma~1),
                        mask = cat_mask,
                        detectfn = 1, # hazard rate
                        CL = F,
-                       verify = F) # ---> best detection function
-
+                       verify = F,
+                       details = list(fastproximity = FALSE)) # ---> best detection function
+# summary(fit0)
+# print(fit0)
 fit00 <- secr::secr.fit(CM,
                        model = list(D~1, g0~1, sigma~1),
                        mask = cat_mask,
                        detectfn = 0, # half-normal
                        CL = F,
-                       verify = F)
+                       verify = F,
+                       details = list(fastproximity = FALSE))
+summary(fit00)
 
 AIC(fit0, fit00)
 
 #                      model    detectfn npar    logLik     AIC    AICc dAICc AICcwt
-# fit0  D~1 g0~1 sigma~1 z~1 hazard rate    4 -146.4083 300.817 308.817 0.000 0.7342
-# fit00     D~1 g0~1 sigma~1  halfnormal    3 -150.4243 306.849 310.849 2.032 0.2658
+# fit0  D~1 g0~1 sigma~1 z~1 hazard rate    4 -310.5827 629.165 637.165 0.000 0.7394
+# fit00     D~1 g0~1 sigma~1  halfnormal    3 -314.6254 635.251 639.251 2.086 0.2606
+# Warning message:
+# In AIC.secr(fit0, fit00) : models not compatible for AIC
 
 ###################################################
 # Covariate 'hab_type' (cloded vs. open) effect ####
 ###################################################
-
+print("fit0")
 fit1 <- secr::secr.fit(CM,
                        model = list(D~1, g0~1, sigma~1),
                        mask = cat_mask,
                        detectfn = 1, # hazard rate
                        CL = F,
-                       verify = F)
+                       verify = F,
+                       details = list(fastproximity = FALSE))
 summary(fit1)
+print("fit2")
 fit2 <- secr::secr.fit(CM,
                        model = list(D~1, g0~hab_type, sigma~1),
                        mask = cat_mask,
                        detectfn = 1, # hazard rate
                        CL = F,
-                       verify = F)
-
+                       verify = F,
+                       details = list(fastproximity = FALSE))
+print("fit3")
 fit3 <- secr::secr.fit(CM,
                        model = list(D~1, g0~1, sigma~hab_type),
                        mask = cat_mask,
                        detectfn = 1, # hazard rate
                        CL = F,
-                       verify = F)
+                       verify = F,
+                       details = list(fastproximity = FALSE))
+print("fit4")
 fit4 <- secr::secr.fit(CM,
                        model = list(D~1, g0~hab_type, sigma~hab_type),
                        mask = cat_mask,
                        detectfn = 1, # hazard rate
                        CL = F,
-                       verify = F)
-
+                       verify = F,
+                       details = list(fastproximity = FALSE))
+summary(fit4)
 AIC(fit1,
     fit2,
     fit3,
     fit4)
 
-#                     model    detectfn npar    logLik     AIC    AICc  dAICc AICcwt
-# fit1               D~1 g0~1 sigma~1 z~1 hazard rate    4 -146.4083 300.817 308.817   0.000      1
-# fit2        D~1 g0~hab_type sigma~1 z~1 hazard rate    6 -139.8154 291.631 319.631  10.814      0
-# fit3        D~1 g0~1 sigma~hab_type z~1 hazard rate    6 -142.3548 296.710 324.710  15.893      0
-# fit4 D~1 g0~hab_type sigma~hab_type z~1 hazard rate    8 -136.9045 289.809 433.809 124.992      0
+# Warning message:
+# In secr::secr.fit(CM, model = list(D ~ 1, g0 ~ hab_type, sigma ~  :
+#   at least one variance calculation failed 
+#                                   model    detectfn npar    logLik     AIC    AICc   dAICc AICcwt
+# fit1               D~1 g0~1 sigma~1 z~1 hazard rate    4 -310.5827 629.165 637.165   0.000      1
+# fit2        D~1 g0~hab_type sigma~1 z~1 hazard rate    6 -304.0432 620.086 648.086  10.921      0
+# fit3        D~1 g0~1 sigma~hab_type z~1 hazard rate    6 -306.5353 625.071 653.071  15.906      0
+# fit4 D~1 g0~hab_type sigma~hab_type z~1 hazard rate    8 -301.0773 618.155 762.155 124.990      0
 
 summary(fit1)
 
 # $versiontime
-# [1] "4.4.5, run 16:29:11 09 mars 2022, elapsed 11.78 s"
+# [1] "4.4.5, run 10:50:52 10 mars 2022, elapsed 470.61 s"
 
 # $traps
 #  Detector Number  Spacing UsagePct
-#     count     41 126.6491 2412.195
+#     count     41 126.6491 38.28881
 
 # $capthist
-#  Occasions Detections    Animals  Detectors      Moves
-#          1         60         10         41         21 
+#  Occasions Detections    Animals  Detectors      Moves 
+#         63         60         10         41         33 
 
 # $mask
 #  Cells  Spacing     Area
@@ -206,20 +213,20 @@ summary(fit1)
 
 # $AICtable
 #                 model    detectfn npar    logLik     AIC    AICc
-#  D~1 g0~1 sigma~1 z~1 hazard rate    4 -146.4083 300.817 308.817
+#  D~1 g0~1 sigma~1 z~1 hazard rate    4 -310.5827 629.165 637.165
 
 # $coef
 #            beta   SE.beta       lcl       ucl
-# D     -5.888997 0.3425120 -6.560308 -5.217686
-# g0    -2.814714 0.2536359 -3.311832 -2.317597
-# sigma  6.749905 0.1827173  6.391786  7.108025
-# z      1.341887 0.1642288  1.020004  1.663769
+# D     -5.889026 0.3425030 -6.560320 -5.217733
+# g0    -2.784541 0.2619698 -3.297993 -2.271090
+# sigma  6.745073 0.1842882  6.383875  7.106272
+# z      1.344216 0.1643313  1.022133  1.666300
 
 # $predicted
 #        link     estimate  SE.estimate          lcl          ucl
-# D       log 2.769754e-03 9.771892e-04   0.00141545 5.419857e-03
-# g0    logit 5.653420e-02 1.352846e-02   0.03516752 8.967603e-02
-# sigma   log 8.539779e+02 1.573480e+02 596.92166051 1.221732e+03
-# z       log 3.826255e+00 6.326421e-01   2.77320624 5.279171e+00
+# D       log 2.769673e-03 9.771335e-04 1.415433e-03 5.419604e-03
+# g0    logit 5.816528e-02 1.435125e-02 3.564012e-02 9.354575e-02
+# sigma   log 8.498616e+02 1.579587e+02 5.922182e+02 1.219592e+03
+# z       log 3.835180e+00 6.345192e-01 2.779116e+00 5.292549e+00
 
-# save.image(file = "my_work_space.RData")
+# save.image(file = "my_work_space2.RData")
