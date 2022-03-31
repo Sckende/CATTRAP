04ed67d56a4e4c36e78f6aab4a5f1d3538f45785
Sckende <clrccljuhasz@gmail.com>
2022-03-31 15:44:35 +0400

    # Distance chats capturés & caméras

--- Carto_CATTRAP.R
+++ Carto_CATTRAP.R
@@ -53,3 +53,24 @@ date(cat.sp$date_capt[cat.sp$zone == 'MAI'])
 # mapshot(map,
 #         file = 'C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/2-CHAT_optimis_piegeage_genet/DATA/map_test.png',
 #         remove_controls = c("homeButton", "layersControl"))
+
+#### distance entre les cam et les 4 chats capturés 6 mois avant l'étude ####
+grid <- read.table("C:/Users/ccjuhasz/Desktop/SMAC/GITHUB/CATTRAP/Claire_script_secr/CATTRAP_grid.txt",
+                   head = T,
+                   sep = '\t',
+                   dec = ',')
+
+projUTM <- '+init=epsg:32740'
+grid.sp <- st_as_sf(grid,
+                    coords = c('X', 'Y'),
+                    crs = projUTM)
+cat.sp2 <- cat.sp[year(cat.sp$date_capt) == 2015 & month(cat.sp$date_capt) %in% 9:12 & cat.sp$zone == "MAI", ]
+
+min(st_distance(cat.sp2[1,],
+            grid.sp)) # 5925.35 m
+min(st_distance(cat.sp2[2,],
+            grid.sp)) # 7798.253 m
+min(st_distance(cat.sp2[3,],
+            grid.sp)) # 6222.516 m
+min(st_distance(cat.sp2[4,],
+            grid.sp)) # 6188.725 m
